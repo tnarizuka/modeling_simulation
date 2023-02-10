bd16671b42e6ef470cad54efbaaa956d4f4c79d6
narizuka <pararel@gmail.com>
2023-02-10 11:33:10 +0900

    updates

--- build_push.sh
+++ build_push.sh
@@ -3,8 +3,8 @@
 # Jupyter Bookの内容を更新
 jb build --all .
 git add .
-git commit -m "updates"  
+git commit -m "updates"
 git push origin main
 
 # GitHub Pagesの更新
-ghp-import -n -p -f _build/html 
+ghp-import -n -p -f _build/html
