d08b724de2c6fe270ed5725eb22aea07f904ce7c
narizuka <pararel@gmail.com>
2024-06-28 12:48:28 +0900

    updates

--- _build/jupyter_execute/chap_4/4_stochastic_model.py
+++ _build/jupyter_execute/chap_4/4_stochastic_model.py
@@ -3,7 +3,7 @@
 
 # # 確率モデル
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -541,28 +541,32 @@ ax.legend(numpoints=1, fontsize=10, loc='upper left', frameon=True, bbox_to_anch
 # 確率変数列 $ U_{1}, U_{2},\ldots, U_{n} $ が成功確率 $ p $ のベルヌーイ分布に従うとき，期待値は $ p $，分散は $ p(1-p) $である．
 # よって，中心極限定理によると，標本平均の分布は $ n $ を大きくしたときに正規分布 $ N(p, p(1-p)/n) $ に近づく．
 
-# In[63]:
+# In[190]:
 
 
-fig, ax = plt.subplots(figsize=(5, 4))
-
-# 様々な標本サイズnに対して標本平均のヒストグラムを描画
+# 標本平均を10000回計算してヒストグラムを描画（標本数）
 p=1/6
-for n in [5, 20, 100]:
+T10, T20, T50 = [], [], []
+for j in range(10000):
+    U10 = sp.stats.bernoulli.rvs(p, size=10) # 確率pのベルヌーイ分布からサイズnの標本を生成
+    U20 = sp.stats.bernoulli.rvs(p, size=20) # 確率pのベルヌーイ分布からサイズnの標本を生成
+    U50 = sp.stats.bernoulli.rvs(p, size=50) # 確率pのベルヌーイ分布からサイズnの標本を生成
+    T10.append(U10.mean())
+    T20.append(U20.mean())
+    T50.append(U50.mean())
 
-    # 標本平均を1000回計算してヒストグラムを描画（標本数）
-    T = []
-    for j in range(1000):
-        U = sp.stats.bernoulli.rvs(p, size=n) # 確率pのベルヌーイ分布からサイズnの標本を生成
-        T.append(U.mean())
-    ax.hist(T, bins=10, density=1, edgecolor='w', alpha=0.5, label='$n=%s$' % n); 
+# 標本平均のヒストグラムを描画
+fig, ax = plt.subplots(figsize=(5, 4))
+ax.hist(T10, bins=np.unique(T10), align='left', rwidth=0.2, density=1, edgecolor='w', alpha=0.5, label='$n=10$')
+ax.hist(T20, bins=np.unique(T20), align='left', rwidth=0.2, density=1, edgecolor='w', alpha=0.5, label='$n=20$')
+ret = ax.hist(T50, bins=np.unique(T50), align='left', rwidth=1, density=1, edgecolor='w', alpha=0.3, label='$n=50$')
 
 # 正規分布N(\mu, \sigma^2/n)の確率密度関数を描画
 t = np.arange(0, 1, 0.001)
-gt = sp.stats.norm.pdf(t, loc=p, scale=np.sqrt(p*(1-p)/n))
-ax.plot(t, gt, 'k--', lw=1.2, label='$N(p, p(1-p)/n)$')
+gt = sp.stats.norm.pdf(t, loc=p, scale=np.sqrt(p*(1-p)/50))
+ax.plot(t, gt, 'k--', lw=1, label='$N(p, p(1-p)/n)$')
 
-ax.set_xlim(0, 1), ax.set_ylim(0, 12)
+ax.set_xlim(0, 0.6), ax.set_ylim(0, 8)
 ax.set_xlabel('標本平均 $t$', fontsize=12)
 ax.set_ylabel('$g(t)$', fontsize=15)
 ax.legend(numpoints=1, fontsize=10, loc='upper right', frameon=True);
@@ -573,26 +577,29 @@ ax.legend(numpoints=1, fontsize=10, loc='upper right', frameon=True);
 # 確率変数列 $ U_{1}, U_{2},\ldots, U_{n} $ がパラメータ $ \lambda $ の指数分布に従うとき，期待値は $ \lambda $，分散は $ \lambda^2 $ である．
 # よって，中心極限定理によると，標本平均の分布は $ n $ を大きくしたときに正規分布 $ N(\lambda, \lambda^2/n) $ に近づく．
 
-# In[40]:
+# In[221]:
 
+
+# 標本平均を1000回計算してヒストグラムを描画（標本数）
+lmd=2
+T3, T10, T50 = [], [], []
+for j in range(10000):
+    U3 = sp.stats.expon.rvs(loc=0, scale=lmd, size=3) # パラメータlmdのポアソン分布からサイズnの標本を生成
+    U10 = sp.stats.expon.rvs(loc=0, scale=lmd, size=10) # パラメータlmdのポアソン分布からサイズnの標本を生成
+    U50 = sp.stats.expon.rvs(loc=0, scale=lmd, size=50) # パラメータlmdのポアソン分布からサイズnの標本を生成
+    T3.append(U3.mean())
+    T10.append(U10.mean())
+    T50.append(U50.mean())
 
 fig, ax = plt.subplots(figsize=(5, 4))
-
-# 様々な標本サイズnに対して標本平均のヒストグラムを描画
-lmd=2
-for n in [1, 3, 10, 50]:
-
-    # 標本平均を1000回計算してヒストグラムを描画（サンプルサイズ1000）
-    T = []
-    for j in range(1000):
-        U = sp.stats.expon.rvs(loc=0, scale=lmd, size=n) # パラメータlmdのポアソン分布からサイズnの標本を生成
-        T.append(U.mean())
-    ax.hist(T, bins=20, density=1, edgecolor='w', alpha=0.5, label='$n=%s$' % n); 
+ax.hist(T3, bins=20, rwidth=0.2, density=1, edgecolor='w', alpha=0.5, label='$n=10$')
+ax.hist(T10, bins=30, rwidth=0.4, density=1, edgecolor='w', alpha=0.5, label='$n=20$')
+ret = ax.hist(T50, bins=50, rwidth=1, density=1, edgecolor='w', alpha=0.3, label='$n=50$')
 
 # 正規分布N(\mu, \sigma^2/n)の確率密度関数を描画
 t = np.arange(0, 10, 0.01)
 gt = sp.stats.norm.pdf(t, loc=lmd, scale=np.sqrt(lmd**2/n))
-ax.plot(t, gt, 'g-', label='$N(\lambda, \lambda/n)$')
+ax.plot(t, gt, 'k--', lw=1, label='$N(\lambda, \lambda/n)$')
 
 ax.set_xlim(0, 6)
 ax.set_xlabel('標本平均 $t$', fontsize=12)
@@ -600,7 +607,7 @@ ax.set_ylabel('$g(t)$', fontsize=15)
 ax.legend(numpoints=1, fontsize=10, loc='upper right', frameon=True);
 
 
-# ### 演習問題
+# ### サイコロを投げる実験
 # 
 # サイコロを $ n $ 回投げて1が出たら成功，それ以外は失敗とする．
 # これは，成功確率 $ p=1/6 $ のベルヌーイ過程となるので，$ n $ を大きくすると成功割合（1が出た割合）は理論値 $ p $ に近づく（大数の法則）．
